89221f71d42c34cd969ca9a0978f709544767938
katya20 <kpanchenko21@gmail.com>
2016-11-29 08:56:36 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ $(function() {
 function dealWithIt() {
     $("#glasses").animate({top: "250"}, 1500, function () {
       console.log("with");
-      button.html("hi");
-
+      button.val("Reset");
     })
     console.log("deal");
 }
